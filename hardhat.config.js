5ac1dbe29a3fe819027ad2057d601da23f943225
Kabbo | Frontend Developer <160781484+siyam1100@users.noreply.github.com>
2026-02-13 14:09:27 +0600

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,12 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    hardhat: {
+      forking: {
+        url: "https://eth-mainnet.g.alchemy.com/v2/YOUR_API_KEY", // Forking Mainnet for testing
+      }
+    }
+  }
+};
